3262e4a9f7fea3459225d0f07ed6cb24dfd44fa0
Bassam Ismail <skippednote@gmail.com>
2014-06-02 08:34:51 +0530

    Update after adding PPA

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,6 +6,8 @@ apt-get install -y vim curl python-software-properties
 
 add-apt-repository -y ppa:ondrej/php5
 
+apt-get update
+
 apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core drush
 
 apt-get install -y php5-xdebug
@@ -19,18 +21,14 @@ EOF
 
 a2enmod rewrite
 
-
 rm -rf /var/www
 ln -fs /vagrant/public /var/www
-
 
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 
 
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-
-
 
 service apache2 restart
 
