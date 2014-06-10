d6b1e4a37181618b6225a6419a2f3dc1dbf443cc
Bassam Ismail <skippednote@gmail.com>
2014-06-10 09:19:51 +0530

    Update gulpfile and add Vagrantfile

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,24 +1,33 @@
-var config           = require('./config.json');
+var config       = require('./config.json');
 
-var gulp             = require('gulp');
-var sass             = require('gulp-ruby-sass');
-var autoprefixer     = require('gulp-autoprefixer');
-var csscomb          = require('gulp-csscomb');
-var minify           = require('gulp-minify-css');
-var rename           = require('gulp-rename');
-var concat           = require('gulp-concat');
-var jshint           = require('gulp-jshint');
-var uglify           = require('gulp-uglify');
-var imagemin         = require('gulp-imagemin');
-var clean            = require('gulp-clean');
-var pngcrush         = require('imagemin-pngcrush');
-var newer            = require('gulp-newer');
-var livereload       = require('gulp-livereload');
-var notify           = require('gulp-notify');
-livereload.listen();
+var gulp         = require('gulp');
+var sass         = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var csscomb      = require('gulp-csscomb');
+var minify       = require('gulp-minify-css');
+var rename       = require('gulp-rename');
+var concat       = require('gulp-concat');
+var jshint       = require('gulp-jshint');
+var uglify       = require('gulp-uglify');
+var imagemin     = require('gulp-imagemin');
+var clean        = require('gulp-clean');
+var pngcrush     = require('imagemin-pngcrush');
+var newer        = require('gulp-newer');
+var plumber      = require('gulp-plumber');
+var browserSync  = require('browser-sync');
+var notify       = require('gulp-notify');
+
+
+var onError = function (err) {
+  console.log(err);
+};
+
 
 gulp.task('styles', function() {
     return gulp.src(config.devPath.sass)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(sass({
             style: 'expanded',
             compass: true
@@ -29,13 +38,17 @@ gulp.task('styles', function() {
         .pipe(minify())
         .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest(config.buildPath.styles))
-        .pipe(livereload())
+        .pipe(browserSync.reload({stream:true, once: true}))
+        // .pipe(livereload())
         .pipe(notify({ message: 'Styles task complete' }));
 });
 
 
 gulp.task('scripts', function() {
     return gulp.src(config.devPath.scripts)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(jshint('./.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'))
@@ -44,32 +57,44 @@ gulp.task('scripts', function() {
         .pipe(rename({suffix: ".min"}))
         .pipe(uglify())
         .pipe(gulp.dest(config.buildPath.scripts))
-        .pipe(livereload())
         .pipe(notify({ message: 'Scripts task complete' }));
 });
 
+
 gulp.task('images', function () {
     return gulp.src(config.devPath.images)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(imagemin({
             optimizationLevel: 7,
             progressive: true,
             interlaced: true
         }))
         .pipe(gulp.dest(config.buildPath.images))
-        .pipe(livereload())
         .pipe(notify({ message: 'Images task complete' }));
 });
+
 
 gulp.task('clean', function() {
   return gulp.src([config.buildPath.styles, config.buildPath.scripts, config.buildPath.images], {read: false})
     .pipe(clean());
 });
 
+
 gulp.task('default', ['clean'], function() {
     gulp.start('styles', 'scripts', 'images');
 });
 
-gulp.task('watch', function() {
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+      proxy: "127.0.0.1"
+    });
+});
+
+
+gulp.task('watch', ['browser-sync'], function() {
     gulp.watch(config.devPath.scripts, ['scripts']);
     gulp.watch(config.devPath.sass, ['styles']);
 });
